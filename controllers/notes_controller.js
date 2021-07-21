5c9fb0c41aa3a3a02110c073ec3e263276fdc838
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-07-21 17:41:59 -0400

    Update note working

--- controllers/notes_controller.js
+++ controllers/notes_controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import Note from "../models/note_model.js"
 import User from "../models/user_model.js"
 
@@ -21,4 +22,22 @@ export const createNote = async (req, res) => {
     } catch (error) {
         res.status(409).json({ error: error.message })
     }
+}
+
+export const updateNote = async (req, res) => {
+    const { id: _id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("Note not found")
+
+    const updatedNote = await Note.findByIdAndUpdate(_id, req.body, { new: true })
+    const user = await User.findById(req.body.user)
+    user.notes = [...user.notes.map((note) => {
+        if (note._id.toString() === updatedNote._id.toString()) {
+            return updatedNote
+        } else {
+            return note
+        }
+    })]
+    await user.save()
+    res.status(200).json(updatedNote)
 }
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getNotes, createNote } from "../controllers/notes_controller.js"
+import { getNotes, createNote, updateNote } from "../controllers/notes_controller.js"
 
 const router = express.Router()
 
 router.get('/', getNotes)
 router.post('/', createNote)
+router.patch('/:id', updateNote)
 
 export default router
