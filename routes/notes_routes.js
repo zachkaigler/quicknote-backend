423070a56a88a7ff39ffc432119a8a3887c399d4
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-07-27 19:31:31 -0400

    More troubleshoot

--- routes/notes_routes.js
+++ routes/notes_routes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { getNotes, createNote, updateNote, deleteNote } from "../controllers/notes_controller.js"
+import { createNote, updateNote, deleteNote } from "../controllers/notes_controller.js"
 import auth from "../middleware/auth.js"
 
 const router = express.Router()
 
-router.get('/', getNotes)
+// router.get('/', getNotes)
 router.post('/', auth, createNote)
 router.patch('/:id', auth, updateNote)
 router.delete('/:id', auth, deleteNote)
